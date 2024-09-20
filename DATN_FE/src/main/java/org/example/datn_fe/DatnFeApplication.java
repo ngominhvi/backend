e73febccb2c78c029d7bca47af32ac0c28d2948d
vinmpc07806 <minhvi171218@gmail.com>
2024-09-20 21:33:00 +0700

    first commit

---
+++ DATN_FE/src/main/java/org/example/datn_fe/DatnFeApplication.java
@@ -0,0 +1,13 @@
+package org.example.datn_fe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatnFeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DatnFeApplication.class, args);
+	}
+
+}
---
+++ DATN_FE/src/test/java/org/example/datn_fe/DatnFeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.datn_fe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatnFeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
